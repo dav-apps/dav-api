91dc5c5487126d4a39551e5ca0e4926edece8eb0
David <zurek.dav@gmail.com>
2024-03-04 00:13:33 +0100

    Implement functionality for send notifications task
    - Install web-push to send notifications
    - Install luxon to calculate the new time of notifications with interval

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,9 +1,72 @@
 import nodeCron from "node-cron"
+import webPush from "web-push"
+import { DateTime } from "luxon"
+import { prisma } from "../server.js"
+
+webPush.setVapidDetails(
+	"mailto:support@dav-apps.tech",
+	process.env.WEBPUSH_PUBLIC_KEY,
+	process.env.WEBPUSH_PRIVATE_KEY
+)
 
 export function setupTasks() {
 	nodeCron.schedule("*/10 * * * *", sendNotifications)
 }
 
-function sendNotifications() {
-	console.log("Send notifications")
+async function sendNotifications() {
+	const notifications = await prisma.notification.findMany({
+		where: { time: { lte: new Date() } },
+		include: {
+			user: {
+				include: { sessions: { include: { webPushSubscriptions: true } } }
+			}
+		}
+	})
+
+	for (let notification of notifications) {
+		if (notification.title == null || notification.body == null) {
+			await prisma.notification.delete({ where: { id: notification.id } })
+			continue
+		}
+
+		// Send the notification to all web push subscriptions of the user
+		for (let session of notification.user.sessions) {
+			for (let webPushSubscription of session.webPushSubscriptions) {
+				try {
+					await webPush.sendNotification(
+						{
+							endpoint: webPushSubscription.endpoint,
+							keys: {
+								p256dh: webPushSubscription.p256dh,
+								auth: webPushSubscription.auth
+							}
+						},
+						JSON.stringify({
+							title: notification.title,
+							body: notification.body
+						})
+					)
+				} catch (error) {
+					// Delete the web push subscription
+					await prisma.webPushSubscription.delete({
+						where: { id: webPushSubscription.id }
+					})
+				}
+			}
+		}
+
+		if (notification.interval > 0) {
+			// Update the notification time
+			let newNotificationTime = DateTime.fromJSDate(notification.time)
+				.plus({ seconds: notification.interval })
+				.toJSDate()
+
+			await prisma.notification.update({
+				where: { id: notification.id },
+				data: { time: newNotificationTime }
+			})
+		} else {
+			await prisma.notification.delete({ where: { id: notification.id } })
+		}
+	}
 }

14e3b163457ea821326848e6829c98128f441015
David <zurek.dav@gmail.com>
2024-12-08 01:17:30 +0100

    Add link to logo

--- src/emails/components.tsx
+++ src/emails/components.tsx
@@ -3,11 +3,14 @@ import { Img } from "@react-email/components"
 
 export function Logo() {
 	return (
-		<Img
-			alt="dav Logo"
-			height={50}
-			src="https://dav-misc.fra1.cdn.digitaloceanspaces.com/dav-logo.png"
-			style={{ marginLeft: "auto", marginRight: "auto" }}
-		/>
+		<div style={{ display: "flex", justifyContent: "center" }}>
+			<a href="https://dav-apps.tech" target="_blank">
+				<Img
+					src="https://dav-misc.fra1.cdn.digitaloceanspaces.com/dav-logo.png"
+					height={50}
+					alt="dav Logo"
+				/>
+			</a>
+		</div>
 	)
 }
